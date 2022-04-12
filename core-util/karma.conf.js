b77b4c04ead8efec88d2e223a08af37d155d2399
Roman Bondarev <roman.bondarev@kodality.com>
2022-04-12 13:08:40 +0300

    update .gitlab-ci.yml

--- core-util/karma.conf.js
+++ core-util/karma.conf.js
@@ -38,6 +38,17 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });

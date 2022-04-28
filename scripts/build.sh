86d67124abb6a4f523ce2cafd434d84bc68384de
Daniel <daniel@kodality.com>
2022-04-28 15:01:45 +0300

    build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd `dirname $0`/..
+set -e
 
 rm -rf dist
 
 for d in core-util; do
   echo "Building $d"
-  ng build $d || exit 1
+  ng build $d
   rm -rf node_modules/@kodality-web/$p && cp -r dist/$p node_modules/@kodality-web/$p
 done
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 cd `dirname $0`/..
-VERSION=$1
+set -e
 
-declare -A package_versions
+VERSION=$1
+if [ -z $VERSION ]; then
+  v=$(cat package.json | sed -n 's/^ *"version": "\(.*\)",/\1/p')
+  ts=$(date +%s)
+  VERSION=$(echo "$v" | sed "s/.0.0$/.0.0-$ts/g")
+fi
+[[ -z VERSION ]] && echo "give me a version" && exit 1
+echo "publishing $VERSION"
 
 for p in core-util; do
-  cd $p
-
-  if [ -z $VERSION ]; then
-    v=$(cat package.json | sed -n 's/^ *"version": "\(.*\)",/\1/p')
-    ts=$(date +%s)
-    version=$(echo "$v" | sed "s/.0.0$/.0.0-SNAPSHOT.$ts/g")
-    package_versions[$p]=$version
-  else
-    package_versions[$p]=$VERSION
-  fi
-
-  [[ -z "${package_versions[$p]}" ]] && echo "give me a version" && exit 1
-  cd -
-done
-
-
-for key in ${!package_versions[@]}; do
-  cd dist/$key
-
-  VERS_JSON="\"version\": \"${package_versions[$key]}\","
-  sed -i -e "s/\"version\": \"\(.*\)\",/$VERS_JSON/" package.json
-
-  echo "package(${key}): publish ${package_versions[$key]}";
-
+  cd dist/$p
+  sed -i 's/^ *"version": ".*",//g' package.json #delete version if any
+  sed -i "2 i \"version\": \"$VERSION\"," package.json
   npm publish
   cd -
 done

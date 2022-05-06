26b521aaf5cc5c5f084b25f9f06275422a27c09c
Roman Bondarev <roman.bondarev@kodality.com>
2022-05-06 14:37:12 +0300

    fix args

--- core-util/lib/pipes/common/apply.pipe.ts
+++ core-util/lib/pipes/common/apply.pipe.ts
@@ -4,7 +4,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'apply'})
 export class ApplyPipe implements PipeTransform {
   // todo: add generics
-  public transform(value: any, fun: (f: any, ...arg: any) => any, ...fnArg: any): any {
+  public transform(value: any, fun: (f: any, ...arg: any[]) => any, ...fnArg: any[]): any {
     return fun?.(value, ...fnArg) ?? value;
   }
 }

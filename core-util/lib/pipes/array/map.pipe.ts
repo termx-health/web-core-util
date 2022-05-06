2bcf93e5fa6ff5bf423e8b2b708227b74ecadf3e
Roman Bondarev <roman.bondarev@kodality.com>
2022-05-06 14:23:58 +0300

    pipes

--- core-util/lib/pipes/array/map.pipe.ts
+++ core-util/lib/pipes/array/map.pipe.ts
@@ -3,7 +3,7 @@ import {getPathValue, isNil} from '../../utils';
 
 @Pipe({name: 'map'})
 export class MapPipe implements PipeTransform {
-  public transform<T>(values: T[], mapper: string | ((f: T, arg: any) => boolean), ...fnArg: any): T[] | undefined {
+  public transform<T>(values: T[], mapper: string | ((f: T, ...arg: any) => boolean), ...fnArg: any): T[] | undefined {
     if (isNil(values)) {
       return undefined;
     }

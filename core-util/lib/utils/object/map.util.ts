98075bc4db57cec774214d988dc413e20f3aef40
Roman Bondarev <roman.bondarev@kodality.com>
2022-05-11 14:22:19 +0300

    map.util

--- core-util/lib/utils/object/map.util.ts
+++ core-util/lib/utils/object/map.util.ts
@@ -3,8 +3,10 @@ export function group<T>(array: T[], fn: (el: T) => string | number | symbol): {
 }
 
 export function collect<T>(array: T[], fn: (x: T) => string | number | symbol): {[key: string | number | symbol]: T[]} {
+  const _acc: Record<string | number | symbol, T[]> = {};
+
   return array.reduce((acc, el) => {
     const key = fn(el);
     return ({...acc, [key]: [...(acc[key] || []), el]});
-  }, {}) as Record<string | number | symbol, T[]>;
+  }, _acc) as Record<string | number | symbol, T[]>;
 }

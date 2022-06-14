991e8be6c24b2b26b7ab1efe87b4f2352cb6f362
Marina Ivanova <marina.ivanova@kodality.com>
2022-06-14 12:03:16 +0300

    http cache service

---
+++ core-util/lib/services/http-cache.service.ts
@@ -0,0 +1,15 @@
+import {Observable, shareReplay} from 'rxjs';
+import {Injectable} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class HttpCacheService {
+  private cache: {[key: string]: Observable<any>} = {};
+
+  public getCachedResponse(key: string, request: Observable<any>): Observable<any> {
+      if (!this.cache[key]) {
+      this.cache[key] = request.pipe(shareReplay(1));
+    }
+    return this.cache[key];
+  }
+
+}
--- core-util/lib/services/public-api.ts
+++ core-util/lib/services/public-api.ts
@@ -2,3 +2,4 @@ export * from './print.service';
 export * from './component-state.store';
 export * from './local-storage.service';
 export * from './session-storage.service';
+export * from './http-cache.service';

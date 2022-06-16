d5d0eb4d1e0a57fc59c692de4552998d226c55c1
Roman Bondarev <roman.bondarev@kodality.com>
2022-06-16 08:35:04 +0300

    clipboard service

--- core-util/lib/services/clipboard.service.ts
+++ core-util/lib/services/clipboard.service.ts
@@ -1,79 +1,40 @@
-import {Inject, Injectable, OnDestroy} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
-import {BehaviorSubject, Subject, takeUntil} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class ClipboardService implements OnDestroy {
-  private queue: {content: string, notifier: BehaviorSubject<boolean>}[] = [];
-  private queueProcessor$ = new Subject<void>();
-  private destroy$ = new Subject<void>();
-
-  private tempTextArea: HTMLTextAreaElement | undefined;
+export class ClipboardService {
   private window: Window;
 
-  public constructor(
-    @Inject(DOCUMENT) public document: Document
-  ) {
+  public constructor(@Inject(DOCUMENT) public document: Document) {
     this.window = document.defaultView as Window;
-    this.initQueue();
   }
 
   // todo: fix deprecated methods & check browser support/platform
 
-  private initQueue(): void {
-    let processing = false;
-    this.queueProcessor$.pipe(takeUntil(this.destroy$)).subscribe(() => {
-      if (processing || this.queue.length === 0) {
-        return;
-      }
-
-      const {content, notifier} = this.queue.shift()!;
-      const didCopy = this.copyFromContent(content);
-      notifier.next(didCopy);
-      notifier.complete();
-      processing = false;
-
-      if (this.queue.length) {
-        this.queueProcessor$.next(void 1);
-      }
-    });
+  public copy(content: string): void {
+    this.copyFromContent(content);
   }
 
-  public copy(content: string): Subject<boolean> {
-    const notifier = new BehaviorSubject<boolean>(false);
-    const data = {content, notifier};
-
-    this.queue.push(data);
-    this.queueProcessor$.next(void 1);
-
-    return notifier;
-  }
-
-  public copyFromContent(content: string, container: HTMLElement = this.document.body): boolean {
-    if (!this.tempTextArea) {
-      this.tempTextArea = ClipboardService.createTextArea(this.document, this.window);
-      try {
-        container.appendChild(this.tempTextArea);
-      } catch (error) {
-        throw new Error('Container should be a Dom element');
-      }
+  private copyFromContent(content: string, container: HTMLElement = this.document.body): void {
+    const tempTextArea = ClipboardService.createTextArea(this.document, this.window);
+    try {
+      container.appendChild(tempTextArea);
+    } catch (error) {
+      throw new Error('Container should be a Dom element');
     }
 
     // set value
-    this.tempTextArea.value = content;
-    this.tempTextArea.select();
-    this.tempTextArea.setSelectionRange(0, this.tempTextArea.value.length);
+    tempTextArea.value = content;
+    tempTextArea.select();
+    tempTextArea.setSelectionRange(0, tempTextArea.value.length);
 
     // copy value
     this.document.execCommand('copy');
 
     // restore state
-    this.tempTextArea.focus();
+    tempTextArea.focus();
     this.window.getSelection()?.removeAllRanges();
-    this.destroy(this.tempTextArea.parentElement || undefined);
-
-    this.queueProcessor$.next(void 1);
-    return true;
+    container.removeChild(tempTextArea);
   }
 
   private static createTextArea(doc: Document, window: Window): HTMLTextAreaElement {
@@ -92,17 +53,4 @@ export class ClipboardService implements OnDestroy {
     ta.setAttribute('readonly', '');
     return ta;
   }
-
-  public destroy(container: HTMLElement = this.document.body): void {
-    if (this.tempTextArea) {
-      container.removeChild(this.tempTextArea);
-      this.tempTextArea = undefined;
-    }
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next(void 1);
-    this.destroy$.complete();
-  }
-
 }

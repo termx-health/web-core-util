52f8cc502ab834c428df9f4f303e681d6b46aeca
Roman Bondarev <roman.bondarev@kodality.com>
2023-02-03 13:21:46 +0200

    serializeDate should serialize valid date only

--- core-util/lib/utils/serialization/serialization.util.ts
+++ core-util/lib/utils/serialization/serialization.util.ts
@@ -1,11 +1,11 @@
 import {DateRange, Range} from '../../models';
-import {format, from} from '../date/date.util';
+import {format, from, isValid} from '../date/date.util';
 import {isDefined, isNil} from '../object/object.util';
 
 
 export function serializeDate(date?: Date | string): string | undefined {
   // Wed Jan 22 2022 04:20:00 GMT+0300 becomes '2022-01-12T04:20:00.000'
-  return isDefined(date) ? format(from(date), "yyyy-MM-dd'T'HH:mm:ss") : undefined;
+  return isDefined(date) && isValid(date) ? format(from(date), "yyyy-MM-dd'T'HH:mm:ss") : undefined;
 }
 
 export function serializeDateRange(range: DateRange): Range<string> | undefined {

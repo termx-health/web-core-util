a1fc19586d4dd78fd3f23f336fb0bfb7ac64e42a
Roman Bondarev <roman.bondarev@kodality.com>
2023-03-31 15:26:43 +0300

    generic to cache service

--- core-util/lib/services/http-cache.service.ts
+++ core-util/lib/services/http-cache.service.ts
@@ -5,7 +5,7 @@ import {Injectable} from '@angular/core';
 export class HttpCacheService {
   private cache: {[key: string]: Observable<any>} = {};
 
-  public getCachedResponse(key: string, request: Observable<any>): Observable<any> {
+  public getCachedResponse<T>(key: string, request: Observable<T>): Observable<T> {
       if (!this.cache[key]) {
       this.cache[key] = request.pipe(shareReplay(1));
     }

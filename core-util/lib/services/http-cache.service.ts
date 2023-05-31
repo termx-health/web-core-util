4e5f87a33920e66d8bdc92ff286146df640794c9
Roman Bondarev <roman.bondarev@kodality.com>
2023-05-31 09:55:38 +0300

    add more methods to http cache service

--- core-util/lib/services/http-cache.service.ts
+++ core-util/lib/services/http-cache.service.ts
@@ -5,10 +5,32 @@ import {Injectable} from '@angular/core';
 export class HttpCacheService {
   private cache: {[key: string]: Observable<any>} = {};
 
+  /**
+   * @deprecated use put(key: string, request: Observable<T>)
+   */
   public getCachedResponse<T>(key: string, request: Observable<T>): Observable<T> {
-      if (!this.cache[key]) {
+    console.error('Use put() method instead!');
+    throw this.put(key, request);
+  }
+
+  public get<T>(key: string): Observable<T> {
+    return this.cache[key];
+  }
+
+  public put<T>(key: string, request: Observable<T>): Observable<T> {
+    if (!this.cache[key]) {
       this.cache[key] = request.pipe(shareReplay(1));
     }
     return this.cache[key];
   }
+
+  public remove(key: string): void {
+    delete this.cache[key];
+  }
+
+  public clear(): void {
+    this.cache = {};
+  }
 }
+
+

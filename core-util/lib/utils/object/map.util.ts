d7f2f996bdc2a651c59909980df441b50ff58ce4
Roman Bondarev <roman.bondarev@kodality.com>
2023-09-20 11:59:41 +0300

    group & collect: provide key to transform

--- core-util/lib/utils/object/map.util.ts
+++ core-util/lib/utils/object/map.util.ts
@@ -1,22 +1,25 @@
 export function group<K extends string | number | symbol, V, R = V>(
   array: V[],
   groupBy: (val: V) => K,
-  transform: (val: V) => R = (val): R => val as unknown as R
+  transform: (val: V, key?: K) => R = (val): R => val as unknown as R
 ): Record<K, R> {
-  return array.reduce((acc, val) => ({...acc, [groupBy(val)]: transform(val)}), {}) as Record<K, R>;
+  return array.reduce((acc, val) => {
+    const k = groupBy(val);
+    return ({...acc, [k]: transform(val, k)});
+  }, {}) as Record<K, R>;
 }
 
 
 export function collect<K extends string | number | symbol, V, R = V>(
   array: V[],
   collectBy: (val: V) => K,
-  transform: (val: V) => R = (val): R => val as unknown as R
+  transform: (val: V, key?: K) => R = (val): R => val as unknown as R
 ): Record<K, R[]> {
   const _acc = {} as Record<K, R[]>;
 
   return array.reduce((acc, val) => {
     const k = collectBy(val);
-    const v = transform(val);
+    const v = transform(val, k);
     return ({...acc, [k]: [...(acc[k] || []), v]});
   }, _acc) as Record<K, R[]>;
 }

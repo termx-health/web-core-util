aa10dd8b4fc86775332c87779e3041aa7f108f3e
Roman Bondarev <roman.bondarev@kodality.com>
2023-11-14 14:07:09 +0200

    sort funs

--- core-util/lib/utils/array/sort.util.ts
+++ core-util/lib/utils/array/sort.util.ts
@@ -6,8 +6,17 @@ export function sort<T>(array: T[], key?: string | RecursiveKeyOf<T>, ascending:
     return array;
   }
 
+  return array.sort(sortFn(key, ascending));
+}
+
+export function sortFn<T>(key?: string | RecursiveKeyOf<T>, ascending = true): (a: T, b: T) => number {
+  if (isNil(key)) {
+    return () => 0;
+  }
+
   const sortParams = key.replaceAll(' ', '').split(',');
-  return array.sort((a, b): number => {
+
+  return (a, b): number => {
     for (let param of sortParams) {
       let tempAscending = ascending;
       if (param.startsWith('-')) {
@@ -20,23 +29,30 @@ export function sort<T>(array: T[], key?: string | RecursiveKeyOf<T>, ascending:
       }
     }
     return 0;
-  });
+  };
 }
 
-export function compareValues(val1: any, val2: any, ascending: boolean = true): number {
-  if (typeof val1 === 'string' && typeof val2 === 'string') {
-    return compareStrings(val1, val2, ascending);
-  }
-  if (typeof val1 === 'number' && typeof val2 === 'number') {
-    return compareNumbers(val1, val2, ascending);
-  }
-  if (val1 instanceof Date && val2 instanceof Date) {
-    return compareDates(val1, val2, ascending);
-  }
-  if (isNil(val1)) {
-    return -1;
-  }
-  return 0;
+
+export function compareValues<T>(val1: T, val2: T, ascending: boolean = true): number {
+  return compareValuesFn(ascending)(val1, val2);
+}
+
+export function compareValuesFn<T>(ascending: boolean = true): (val1: T, val2: T) => number {
+  return (val1, val2) => {
+    if (typeof val1 === 'string' && typeof val2 === 'string') {
+      return compareStrings(val1, val2, ascending);
+    }
+    if (typeof val1 === 'number' && typeof val2 === 'number') {
+      return compareNumbers(val1, val2, ascending);
+    }
+    if (val1 instanceof Date && val2 instanceof Date) {
+      return compareDates(val1, val2, ascending);
+    }
+    if (isNil(val1)) {
+      return -1;
+    }
+    return 0;
+  };
 }
 
 export function compareStrings(val1: string, val2: string, ascending: boolean = true): number {

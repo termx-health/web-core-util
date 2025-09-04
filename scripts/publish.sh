e5b586aaa9853d4d7ae0490aa8f3516154e18a6c
Daniel Dubrovski <daniel@kodality.com>
2025-09-04 08:35:32 +0300

    build fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,12 @@ cd `dirname $0`/..
 set -e
 
 VERSION=$1
+TAG=""
 if [ -z $VERSION ]; then
   v=$(cat package.json | sed -n 's/^ *"version": "\(.*\)",/\1/p')
   ts=$(date +%s)
   VERSION=$(echo "$v" | sed "s/.0.0$/.0.0-snapshot.$ts/g")
+  TAG="--tag=snapshot"
 fi
 [[ -z VERSION ]] && echo "give me a version" && exit 1
 echo "publishing $VERSION"
@@ -15,7 +17,7 @@ for p in core-util; do
   cd dist/$p
   sed -i 's/^ *"version": ".*",//g' package.json #delete version if any
   sed -i "2 i \"version\": \"$VERSION\"," package.json
-  npm publish
+  npm publish $TAG
   cd -
 done
 
